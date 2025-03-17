716717b25f429a0dcfc4728546748777d046e507
Nethmini Prabodha <nethunethmini256@gmail.com>
2025-03-17 11:01:33 +0530

    fix

--- src/Components/common/ClientLayout.tsx
+++ src/Components/common/ClientLayout.tsx
@@ -1,22 +1,21 @@
-"use client";
+// "use client";
 
-import React, { useState } from "react";
-import BooksList from "@/Components/BookList/bookList";
-import CategoryBar from "./CategoryBar";
+// import React, { useState } from "react";
+// import CategoryBar from "./CategoryBar";
 
-const ClientLayout = ({ children }: { children: React.ReactNode }) => {
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+// const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+//   const [ setSelectedCategory] = useState<string | null>(null);
 
-  const handleSelectCategory = (category: string | null) => {
-    setSelectedCategory(category);
-  };
+//   const handleSelectCategory = (category: string | null) => {
+//     setSelectedCategory(category);
+//   };
 
-  return (
-    <div>
-      <CategoryBar/>
-      {children}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <CategoryBar/>
+//       {children}
+//     </div>
+//   );
+// };
 
-export default ClientLayout;
+// export default ClientLayout;
